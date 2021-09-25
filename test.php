c639bf22a4465d9755b9abd112c191f81006fbb2
Boris <bseminarioa@uni.pe>
2021-09-24 23:16:29 -0500

    202109242316

--- test.php
+++ test.php
@@ -1,3 +1,18 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="test.css">
+</head>
+<body style="background:red;">
+    <form action="accion.php" method="post">
+     Su nombre: <input type="text" name="nombre" />
+     Su edad: <input type="text" name="edad" />
+     <input type="submit" />
+    </form>
+    
+</body>
+</html>
