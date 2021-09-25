ea512a96b80985f0c2b8b30378dd446780e7fbdb
Boris <bseminarioa@uni.pe>
2021-09-25 02:41:26 -0500

    202109250241

--- test.php
+++ test.php
@@ -9,10 +9,84 @@
 </head>
 <body style="background:red;">
     <form action="accion.php" method="post">
-     Su nombre: <input type="text" name="nombre" />
-     Su edad: <input type="text" name="edad" />
+     <input type="text" name="nombre" placeholder="Nombre"/>
+     <br>
+     <input type="text" name="edad" placeholder="Edad" />
      <input type="submit" />
     </form>
+    <section>
+        <h2>Operadores y sus valore en booleano</h2>
+        <p>Cero 0-><?php var_dump(234); var_dump(0);var_dump(0.44);?></p>
+        <p>Caracteres y cadenas de caracteres:</p>
+        <?php var_dump("asdf");var_dump('26236');  
+        var_dump('a');var_dump("");?>
+    </section>
+    <section style="background:forestgreen;">
+    <?php  
+        $a=1;
+        ++$a;
+        echo "El valor de a es: ".$a;
     
+    ?>
+    </section>
+    <section style="background:navy;">
+    <ol>
+    <?php  
+        for($i=1;$i<5;$i++){
+            
+            echo "<li>Linea $i</li>";
+        }    
+    ?>
+    </ol>
+    </section>
+    <section>
+        <?php
+            $a=5;$b=5;
+            $valor=$a>$b?"mayor":($a<$b?"menor":"igual");
+            echo $valor;
+        ?>
+    </section>
+    <section class="match">
+        <h1>Novedad PHP8: match</h1>
+        <?php
+            $x=5;
+            echo match($x){
+                1=>"Primero",
+                2=>"Segundo",
+                3=>"Tercero",
+                default=>"No tiene merito\n"
+            };
+            echo "  \n";
+            $nombres=['hirsdop','ambedssr','sdsd','ererui'];
+            foreach($nombres as $indice){
+                echo $indice."<br>";
+            }
+            // exit();//Elimina el progrma
+            #klklsd
+            // die();
+        ?>
+    </section>
+    <section class="funciones">
+    <?php
+            echo time()."es el tiempo<br>";
+            echo "Raiz".sqrt(8)."<br>";
+            echo "pi".pi();
+        ?>
+        <h1>Definiendo funciones, ejemplos</h1>
+            <?php
+            function factorial($numero){
+                $resultado=1;
+            for($i=1;$i<=$numero;$i++){
+                $resultado=$resultado*$i;
+            }
+            return $resultado;
+            }
+            echo factorial(7);
+            
+            ?>
+
+
+    </section>
+    <a href="https://youtu.be/cAdBqOI8gwQ?list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&t=274">Curso PHP me quede aqui</a>
 </body>
 </html>
